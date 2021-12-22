b2955b1bbe238ee43c76c7f559c220efd8226515
Bastian <bastian@maramoja.co.ke>
2021-12-22 19:17:12 +0300

    #3: return UTT value on endorsements as number, assuming that amounts won't grow too big.

--- server/service/response.js
+++ server/service/response.js
@@ -4,7 +4,7 @@ export function endorsementsResponse({ endorsementEvents, fromBlock, toBlock }) 
   const endorsements = endorsementEvents.map(endorsementEvent => ({
     source: endorsementEvent.args[0],
     target: endorsementEvent.args[1],
-    value: ethers.utils.formatEther(ethers.BigNumber.from(endorsementEvent.args[3])),
+    value: Number(ethers.utils.formatEther(ethers.BigNumber.from(endorsementEvent.args[3]))),
     block: {
       number: endorsementEvent.blockNumber
     }
