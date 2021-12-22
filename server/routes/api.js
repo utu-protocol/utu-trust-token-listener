555cbb718c3e21d28e597b635fd007f02a708d3e
Bastian <bastian@maramoja.co.ke>
2021-12-22 19:27:49 +0300

    #3: add balance service spec and route

--- server/routes/api.js
+++ server/routes/api.js
@@ -9,6 +9,11 @@ router.get('/', async (req, res) => {
   res.send('UTU Coin Endorses Listener');
 });
 
+/* GET UTT balance for address. */
+router.get('/balance/:address', async (req, res) => {
+  res.send(await balance(req.params.address));
+});
+
 /* GET all endorsements for address. */
 router.get('/endorsements/:target_address?', async (req, res) => {
   res.send(endorsementsResponse(await getEndorsements(req.params.target_address, parseInt(req.query.from_block))));
